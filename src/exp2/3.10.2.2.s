8777770969a96e6354528dfdd731ca18f9d66fe0
lucastrschneider <lucastrschneider@gmail.com>
2022-03-29 04:25:09 -0300

    add: added exercise 3.10.2 answer

---
+++ src/exp2/3.10.2.2.s
@@ -0,0 +1,12 @@
+.text
+.globl main
+main:
+	LDR	r0, =0x2   @ comentarios vem depois de @ ou entre /* ... */		
+	LDR	r1, =0x80000000
+	BL	firstfunc    @ desvia para funcao, coloca o enderenco de retorno em R14 ou LR (link register).		
+    MOV	r0, #0x18		
+	LDR	r1, =0x20026		
+	SWI	0x123456     @ no simulador (target sim) nao existe codigo a ser rodado quando se faz uma SWI sw. interrupt e portanto, gera erro.		
+firstfunc:
+	UMULLS r2, r3, r0, r1   @ O S em ADDS coloca as flags N Z C V no CPSR		
+	MOV	pc, lr	     @ retorna da funcao		
