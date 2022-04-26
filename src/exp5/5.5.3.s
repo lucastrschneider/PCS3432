6ee3ef04e7ec994bf3d202e1590e78d85eb088ca
lucastrschneider <lucastrschneider@gmail.com>
2022-04-26 10:02:31 -0300

    fix: fixed memory usage in 5.5.3.s

--- src/exp5/5.5.3.s
+++ src/exp5/5.5.3.s
@@ -3,7 +3,7 @@
 
 main:
     MOV r5, #11 @ Tamanho do array
-    ADR r0, series @ A série está localizada em r0
+    LDR r0, =dados+4 @ A série está localizada em r0
 
     LDR r1, [r0] @ Guarda o maior valor do array
     MOV r2, #1 @ contador do loop
@@ -25,12 +25,9 @@ loop:
     B loop
 
 done:
-    ADR r0, result
+    ADR r0, dados
     STR r1, [r0]
     B fim
 
-result:
-    .space 4
-
-series:
-    .word 1,28,3,153,5,6,56,8,9,11,10
+dados:
+    .word 0, 1,28,3,153,5,6,56,8,9,11,10
