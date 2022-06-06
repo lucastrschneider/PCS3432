06aa52ba22e3b26060f0e2640b32978a88355409
lucastrschneider <lucastrschneider@gmail.com>
2022-06-06 17:04:45 -0300

    Add exp9 plan

---
+++ src/exp9/plan/test.s
@@ -0,0 +1,170 @@
+	.cpu arm926ej-s
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"test.c"
+	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
+	.align	2
+	.global	c_entry
+	.type	c_entry, %function
+c_entry:
+.LFB0:
+	.file 1 "test.c"
+	.loc 1 1 0
+	.cfi_startproc
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+.LCFI0:
+	.cfi_def_cfa_offset 4
+	.cfi_offset 11, -4
+	add	fp, sp, #0
+.LCFI1:
+	.cfi_def_cfa_register 11
+	.loc 1 2 0
+	mov	r3, #0
+	.loc 1 3 0
+	mov	r0, r3
+	add	sp, fp, #0
+	ldmfd	sp!, {fp}
+	bx	lr
+	.cfi_endproc
+.LFE0:
+	.size	c_entry, .-c_entry
+.Letext0:
+	.section	.debug_info,"",%progbits
+.Ldebug_info0:
+	.4byte	0x41
+	.2byte	0x2
+	.4byte	.Ldebug_abbrev0
+	.byte	0x4
+	.uleb128 0x1
+	.4byte	.LASF0
+	.byte	0x1
+	.4byte	.LASF1
+	.4byte	.LASF2
+	.4byte	.Ltext0
+	.4byte	.Letext0
+	.4byte	.Ldebug_line0
+	.uleb128 0x2
+	.byte	0x1
+	.4byte	.LASF3
+	.byte	0x1
+	.byte	0x1
+	.4byte	0x3d
+	.4byte	.LFB0
+	.4byte	.LFE0
+	.4byte	.LLST0
+	.uleb128 0x3
+	.byte	0x4
+	.byte	0x5
+	.ascii	"int\000"
+	.byte	0
+	.section	.debug_abbrev,"",%progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x1b
+	.uleb128 0xe
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x1
+	.uleb128 0x10
+	.uleb128 0x6
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x2e
+	.byte	0
+	.uleb128 0x3f
+	.uleb128 0xc
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x1
+	.uleb128 0x40
+	.uleb128 0x6
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_loc,"",%progbits
+.Ldebug_loc0:
+.LLST0:
+	.4byte	.LFB0-.Ltext0
+	.4byte	.LCFI0-.Ltext0
+	.2byte	0x2
+	.byte	0x7d
+	.sleb128 0
+	.4byte	.LCFI0-.Ltext0
+	.4byte	.LCFI1-.Ltext0
+	.2byte	0x2
+	.byte	0x7d
+	.sleb128 4
+	.4byte	.LCFI1-.Ltext0
+	.4byte	.LFE0-.Ltext0
+	.2byte	0x2
+	.byte	0x7b
+	.sleb128 4
+	.4byte	0
+	.4byte	0
+	.section	.debug_aranges,"",%progbits
+	.4byte	0x1c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	0
+	.4byte	0
+	.section	.debug_line,"",%progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",%progbits,1
+.LASF3:
+	.ascii	"c_entry\000"
+.LASF1:
+	.ascii	"test.c\000"
+.LASF2:
+	.ascii	"/home/student/src/exp9/plan\000"
+.LASF0:
+	.ascii	"GNU C 4.6.1\000"
+	.ident	"GCC: (Sourcery CodeBench Lite 2011.09-69) 4.6.1"
